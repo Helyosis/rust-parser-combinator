90b8eefadb0aa87b553d8a4252c5e2e51f631cce
Aloïs Colléaux-Le Chêne <alois.colleaux-le-chene@epita.fr>
2022-08-05 21:54:43 +0200

    Add leaf_element that returns an Element object

--- tests/common/element.rs
+++ tests/common/element.rs
@@ -1,10 +1,27 @@
+use std::vec;
+
 use crate::common::*;
 use rust_parser_combinator::*;
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct Element {
+    pub name: String,
+    pub attributes: Vec<(String, String)>,
+    pub children: Vec<Element>,
+}
 
 pub fn element_start<'a>() -> Parser<'a, (String, Vec<(String, String)>)> {
     right(str("<"), pair(wrap_in_spaces(identifier()), attributes()))
 }
 
 pub fn leaf_element_raw<'a>() -> Parser<'a, (String, Vec<(String, String)>)> {
-    left(element_start(), str(">"))
+    left(element_start(), str("/>"))
+}
+
+pub fn leaf_element<'a>() -> Parser<'a, Element> {
+    leaf_element_raw().map(|(n, attr)| Element {
+        name: n,
+        attributes: attr,
+        children: vec![],
+    })
 }
--- tests/element.rs
+++ tests/element.rs
@@ -53,4 +53,30 @@ mod tests {
             .run("<div that's not an identifier>".to_string(), 0)
             .is_err())
     }
+
+    #[test]
+    fn leaf_element_is() {
+        assert_eq!(
+            Ok((
+                37,
+                Element {
+                    name: "div".to_string(),
+                    attributes: vec![
+                        ("x".to_string(), "1".to_string()),
+                        ("y".to_string(), "2".to_string()),
+                        ("z".to_string(), "3".to_string())
+                    ],
+                    children: vec![]
+                }
+            )),
+            leaf_element().run("<div x=\"1\" y = \"2\" z = \"3\"         />".to_string(), 0)
+        )
+    }
+
+    #[test]
+    fn leaf_element_malformed_contain_children() {
+        assert!(leaf_element()
+            .run("<div x=\"1\" >...</div>".to_string(), 0)
+            .is_err())
+    }
 }
