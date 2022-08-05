dd67b0e2e7b601147ca251d6ec3527eaa1daad67
Aloïs Colléaux-Le Chêne <alois.colleaux-le-chene@epita.fr>
2022-08-05 08:58:23 +0200

    Add unit test for whitespace.rs

--- src/string/whitespace.rs
+++ src/string/whitespace.rs
@@ -19,3 +19,42 @@ pub fn whitespaces() -> Parser<'static, String> {
 pub fn whitespaces1() -> Parser<'static, String> {
     many1(whitespace()).map(|v| v.join(""))
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn whitespace_is() {
+        assert_eq!(
+            Ok((1, "\n".to_string())),
+            whitespace().run("\nHello world!".to_string(), 0)
+        )
+    }
+
+    #[test]
+    fn whitespace_is_not() {
+        assert!(whitespace().run("Hello world!".to_string(), 0).is_err())
+    }
+
+    #[test]
+    fn whitespaces_several() {
+        assert_eq!(
+            Ok((3, " \t\n".to_string())),
+            whitespaces().run(" \t\nHey !".to_string(), 0)
+        )
+    }
+
+    #[test]
+    fn whitespaces1_several() {
+        assert_eq!(
+            Ok((3, " \t\n".to_string())),
+            whitespaces1().run(" \t\nHey !".to_string(), 0)
+        )
+    }
+
+    #[test]
+    fn whitespaces1_none() {
+        assert!(whitespaces1().run("Hey !".to_string(), 0).is_err())
+    }
+}
